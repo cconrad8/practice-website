01ff1a6b9f9cbba73be94b0a3f1c15dc3a1360eb
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-10-17 02:18:27 +0000

    test site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,7 +128,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
+        theme: lightCodeTheme
       },
     }),
 };
