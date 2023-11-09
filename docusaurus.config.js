7df64caa99cc66926d77a73bb77b153497b0211f
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-11-09 17:20:53 +0000

    upgrades

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://practice-website-alpha.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/practice-website',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
